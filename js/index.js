942b61babe0a91f7f933a11483435024025bee3a
Dastan-Pazylov <dastanpaz50@gmail.com>
2022-02-22 17:17:13 +1400

    initial

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$('.movie').slick({
+    slidesToShow: 3,
+    dots:true,
+    centerMode: true,
+    centerPadding: '60px',
+})
+
+$('.new').slick({
+    slidesToShow: 4,
+    dots: true,
+})
+
+$('.video').slick({
+    slidesToShow:2,
+    dots:true,
+
+})
+
+$('.casts').slick({
+    slidesToShow: 4,
+
+})
